879d478431dceacfa53789d7fac87619c87867f7
Gabriel Faria <gabrielfaria88@gmail.com>
2024-06-25 10:54:35 -0300

    stylizing profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,32 @@
 import Layout from "../components/Layout/Layout";
 
-
 const Profile = () => {
-    return (
-          <Layout>
-            <h1>Perfil</h1>
-          </Layout>
-      );
-}
+  return (
+    <Layout>
+      <table className="table table-striped">
+        <thead className="text-center">
+          <tr>
+            <th scope="col">Tech</th>
+            <th scope="col">Tipo</th>
+          </tr>
+        </thead>
+        <tbody className="text-center">
+          <tr>
+            <td>React</td>
+            <td>Front-end</td>
+          </tr>
+          <tr>
+            <td>Node</td>
+            <td>Back-end</td>          
+          </tr>
+          <tr>
+            <td>Git</td>
+            <td>Versionamento</td>
+          </tr>
+        </tbody>
+      </table>
+    </Layout>
+  );
+};
 
 export default Profile;
